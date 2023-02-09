a7c51cceb4534c0ffdce31f2a4010bfe8a004bbc
alex.js <zhongwcool@163.com>
2023-02-09 16:48:17 +0800

    增加App00用于测试C#方法

---
+++ App00/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using Mar.Console;
+
+"Hello, World!".PrintMagenta();
+
+const float data = 12.213563135f;
+
+Math.Round(data, 1).ToString(CultureInfo.InvariantCulture).PrintGreen();
